60baa53234181e08ce877f94b075ac56806d9258
yosiaevan <2281020@unai.edu>
2023-10-06 14:56:32 +0700

    deklarasi variabel search dan index, menambahkan algoritma linear search, menampilkan data yang dicari

--- Searching-&-Sorting-Exercise/searching-sorting-1.cpp
+++ Searching-&-Sorting-Exercise/searching-sorting-1.cpp
@@ -13,10 +13,29 @@ int main() {
     // Deklarasi variabel dari struct mahasiswa
     mahasiswa mhs[8] = {{19006,"Matthew",3.15},{19004,"Brad",2.94},{19003,"Anthony",2.78},{19007,"Gillian",3.37},{19005,"Susan",2.93},{19001,"Miya",3.01},{19008,"Alice",3.56},{19002,"Raif",3.44}};
 
+    // Deklarasi variabel search
+    int search, index = 0;
+
     // Menampilkan informasi data mahasiswa
     cout << "NIM\tNama\tIPK" << endl;
-    for (int i = 0; i < 8; i++) {
+    for(int i = 0; i < 8; i++) {
         cout << mhs[i].nim << "\t" << mhs[i].nama << "\t" << mhs[i].ipk << endl;
+    }
+
+    // Linear Searching
+    for(int i = 0; i < 8; i++) {
+        if (mhs[i].nim == search) {
+            index = i;
+            break;
+        }
+    }
+
+    // Menampilkan data yang dicari
+    if(index == 1) {
+        cout << "Data ditemukan:" << endl;
+        cout << "NIM\t: " << mhs[index].nim << endl;
+        cout << "Nama\t: " << mhs[index].nama << endl;
+        cout << "IPK\t: " << mhs[index].ipk << endl;
     }
 
     return 0;
