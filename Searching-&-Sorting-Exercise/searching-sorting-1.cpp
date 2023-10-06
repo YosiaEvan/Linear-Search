95bf44745e9141d533a1800efa2fe04f377c6f78
yosiaevan <2281020@unai.edu>
2023-10-06 14:16:13 +0700

    definisi struktur mahasiswa

---
+++ Searching-&-Sorting-Exercise/searching-sorting-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Definisi struktur dengan nama mahasiswa untuk menyimpan data mahasiswa
+struct mahasiswa{
+    // Member dari struct mahasiswa
+    int nim;
+    char nama[100];
+    float ipk;
+};
+
+int main() {
+
+    return 0;
+}
