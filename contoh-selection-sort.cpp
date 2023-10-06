cc2ac95b9fb926ae0dea684da6bf871714856ae2
yosiaevan <2281020@unai.edu>
2023-10-06 09:21:52 +0700

    membuat fungsi selection sort

--- contoh-selection-sort.cpp
+++ contoh-selection-sort.cpp
@@ -17,6 +17,21 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
+// Fungsi selection sort
+void selectionSort (int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        // Ambil nilai dari i dan simpan sebagai elemen terkecil
+        int min =i;
+        for (int j = i+1; j < size; j++) {
+            // Lakukan perbandingan antara elemen terkecil (min) dengan elemen berikutnya
+            if (array[j] < array[i]) {
+                min = j;
+            }
+            swap(&array[min], &array[i]);
+        }
+    }
+}
+
 int main() {
 
     return 0;
