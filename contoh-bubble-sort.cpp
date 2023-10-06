cc3d9c284b2e44d64fdeed21b3689c0a5df8531c
yosiaevan <2281020@unai.edu>
2023-10-06 08:50:29 +0700

    deklarasi array dan menampilkan array sebelum diurutkan

--- contoh-bubble-sort.cpp
+++ contoh-bubble-sort.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 int main() {
+    // Deklarasi array dengan 6 elemen
+    int array[] = {77, 42, 35, 12, 101, 5};
+
+    // Menampilkan isi array sebelum diurutkan
+    cout << "Elemen sebelum diurutkan:" << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << array[i] << "\t";
+    }
+
     
 
     return 0;
