47336a966e31c05004dbd1920631b8290ed31fc6
yosiaevan <2281020@unai.edu>
2023-10-06 09:14:54 +0700

    membuat fungsi untuk menampilkan isi dari array

---
+++ contoh-selection-sort.cpp
@@ -0,0 +1,16 @@
+// Contoh implementasi algoritma Selection Sort
+
+#include <iostream>
+using namespace std;
+
+// Fungsi untuk menampilkan isi dari array
+void display(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << "\t";
+    }
+}
+
+int main() {
+
+    return 0;
+}
