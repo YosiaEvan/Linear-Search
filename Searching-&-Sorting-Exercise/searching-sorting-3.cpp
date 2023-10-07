0e90668a5050282cf919e1f8345142f1f5e78a1a
yosiaevan <2281020@unai.edu>
2023-10-07 21:00:18 +0700

    menampilkan isi array sebelum diurutkan

--- Searching-&-Sorting-Exercise/searching-sorting-3.cpp
+++ Searching-&-Sorting-Exercise/searching-sorting-3.cpp
@@ -6,7 +6,12 @@ int main() {
     int array[] = {77, 42, 35, 12, 101, 5, 17, 1, 8, 90};
     // Mencari ukuran array
     int size = sizeof(array)/sizeof(array[0]);
-    
+
+    // Menampilkan isi array sebelum diurutkan
+    cout << "Elemen sebelum diurutkan:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << "\t";
+    }
 
     return 0;
 }
