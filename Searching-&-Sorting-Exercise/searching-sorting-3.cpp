a6a597947283a716698fd05345ca30ce75cbe4dc
yosiaevan <2281020@unai.edu>
2023-10-07 20:59:43 +0700

    deklarasi array dan size nya

--- Searching-&-Sorting-Exercise/searching-sorting-3.cpp
+++ Searching-&-Sorting-Exercise/searching-sorting-3.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
+    // Deklarasi array dengan 10 elemen
+    int array[] = {77, 42, 35, 12, 101, 5, 17, 1, 8, 90};
+    // Mencari ukuran array
+    int size = sizeof(array)/sizeof(array[0]);
+    
 
     return 0;
 }
