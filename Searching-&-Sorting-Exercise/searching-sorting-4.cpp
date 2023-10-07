cf814c3e8ed4ff44d7bb8a8dba04ebb086abb0dd
yosiaevan <2281020@unai.edu>
2023-10-07 18:52:11 +0700

    initial commit, deklarasi array 10 elemen

---
+++ Searching-&-Sorting-Exercise/searching-sorting-4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Deklarasi array dengan 10 elemen
+    int array[] = {77, 42, 35, 12, 101, 5, 17, 1, 8, 90};
+
+    // Menampilkan isi array sebelum diurutkan
+    cout << "Elemen sebelum diurutkan:" << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << array[i] << "\t";
+    }
+
+    // Proses pengurutan
+    for (int i = 0; i < 6; i++) {
+        // Pair-wise comparison process
+        for(int j = i+1; j < 6; j++) {
+            // Compare and swap
+            if (array[j] < array[i]) { // "<" untuk ascending dan ">" untuk descending
+                int temp = array[i]; // Temporary variable
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    // Menampilkan elemen setelah pengurutan
+    cout << endl << "Elemen setelah diurutkan:" << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << array[i] << "\t";
+    }
+
+    return 0;
+}
