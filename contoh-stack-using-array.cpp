ba56236b559ebda44b080cd3ebfe905dc3d7af7f
yosiaevan <2281020@unai.edu>
2023-10-20 09:51:24 +0700

    membuat fungsi display dan main

--- contoh-stack-using-array.cpp
+++ contoh-stack-using-array.cpp
@@ -29,7 +29,7 @@ bool isFull() {
 }
 
 // Fungsi untuk mengambil nilai elemen teratas dalam stack
-int totOfStack() {
+int topOfStack() {
     return stack[top];
 }
 
@@ -56,4 +56,56 @@ void pop() {
         top = top - 1; // Atau top--; atau top -= 1;
         cout << temp << " sudah dikeluarkan dari stack." << endl;
     }
+}
+
+// Fungsi untuk menampilkan stack
+void display() {
+    if(isEmpty()) {
+        cout << "Stack kosong." << endl;
+        return;
+    } else {
+        for(int i = top; i >= 0; i++) {
+            cout << endl << stack[i];
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int num; // Variabel yang akan menyimpan nilai yang diinput user
+    int pilihan; // Variabel untuk menyimpan pilihan menu
+    int elemenTop; // Variabel untuk menyimpan elemen teratas dalam top
+
+    do {
+        cout << "===== Menu Stack =====" << endl;
+        cout << "1. Display" << endl
+             << "2. Push" << endl 
+             << "3. Pop" << endl 
+             << "4. Top of Stack" << endl 
+             << "5. Keluar" << endl;
+        cout << "Masukkan pilihan anda: ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            case 1:
+                display();
+                break;
+            case 2:
+                cout << "Input bilangan yang akan ditambahkan ke dalam stack: ";
+                cin >> num;
+                push(num);
+                break;
+            case 3:
+                pop();
+                break;
+            case 4:
+                elemenTop = topOfStack();
+                cout << "Elemen teratas dalam stack saat ini adalah " << elemenTop << endl;
+                break;
+            default:
+                cout << "Pilihan yang anda masukkan salah." << endl;
+        }
+    } while(pilihan != 5);
+
+    return 0;
 }
